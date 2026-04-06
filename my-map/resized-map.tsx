1d6920f8b9b4678d5fdf0fc54f1b6ebe2afec84a
abnaceur <contact@1122.info>
2026-04-06 19:10:47 +0400

    - Add new map

---
+++ my-map/resized-map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1-8-g449813058" name="resized-map" tilewidth="64" tileheight="64" tilecount="4096" columns="64">
+ <image source="../../tilesets/map_resized.png" width="4096" height="4096"/>
+</tileset>
